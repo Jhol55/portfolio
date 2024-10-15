f3c18422618e4c454853604a9f36f5a2900ce9f5
Jhonathan <jhonathan_galhardo@hotmail.com>
2024-10-15 01:27:01 -0300

    Corrigido css do menu de navegação

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -9,15 +9,13 @@ import { StarsBackground } from "@/components/StarsBackground";
 import { ShootingStars } from "@/components/ShootingStars";
 
 
-const Navigation = ({ className, open, isFixed, onClick }: { className?: string, open: boolean, isFixed?: boolean, onClick?: () => void }) => {
+const Navigation = ({ className, onClick }: { className?: string, onClick?: () => void }) => {
     return (
         <>
-            <div className={cn((!isFixed || open) && "!hidden", "h-20 w-full")} />
+            <div className="h-20 w-full" />
             <nav
                 className={cn(
-                    "flex lg:w-fit w-full flex-col lg:flex-row gap-10 items-center",
-                    isFixed && "fixed z-50 bg-[#10071f] rounded-full -translate-y-[4px]",
-                    (isFixed && !open) && "p-3",
+                    "fixed p-3 z-50 bg-[#10071f] rounded-full -translate-y-[4px] flex lg:w-fit w-full flex-col lg:flex-row gap-10 items-center",
                     className)}
             >
                 <div className="flex lg:w-full w-[200px] gap-10 justify-center lg:items-center px-4 flex-col lg:flex-row">
@@ -44,8 +42,6 @@ const Navigation = ({ className, open, isFixed, onClick }: { className?: string,
                         Entre em contato
                     </ShimmerButton>
                 </div>
-                <StarsBackground className={cn(!isFixed && "hidden", "-z-10")} />
-                <ShootingStars className={cn(!isFixed && "hidden", "-z-10")} />
             </nav>
         </>
     )
@@ -55,24 +51,6 @@ const Navigation = ({ className, open, isFixed, onClick }: { className?: string,
 export const NavigationMenu = () => {
     const [open, setOpen] = useState(false);
     const isDesktop = useMediaQuery('(min-width: 1024px)', false);
-    const [isFixed, setIsFixed] = useState(false);
-
-    const handleScroll = () => {
-        if (window.scrollY > 70) {
-            setIsFixed(true);
-        } else {
-            setIsFixed(false);
-        }
-    };
-
-    useEffect(() => {
-        handleScroll();
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-
-    }, []);
 
     useEffect(() => {
         if (isDesktop) {
@@ -80,11 +58,9 @@ export const NavigationMenu = () => {
         }
     }, [isDesktop])
 
-
-
     return (
-        <header className={cn(!isFixed && "p-6", "flex items-center lg:mb-20 lg:justify-center justify-end gap-4")}>
-            <Navigation className="lg:flex hidden" open={open} isFixed={isFixed} />
+        <header className="flex items-center lg:mb-20 lg:justify-center justify-end gap-4">
+            <Navigation className="lg:flex hidden" />
             <Drawer
                 open={!isDesktop && open}
                 onClose={() => setOpen(false)}
@@ -93,10 +69,10 @@ export const NavigationMenu = () => {
                 <DrawerTrigger
                     asChild
                     onClick={() => setOpen(true)}
-                    className={cn(isFixed && "fixed -top-12", "lg:hidden cursor-pointer z-50")}
+                    className="fixed -top-12 lg:hidden cursor-pointer z-50"
                     aria-label="show menu"
                 >
-                    <div className={cn(isFixed && "p-6 mt-12 bg-[#10071f] z-50")} style={{ borderBottomLeftRadius: "50%" }}>
+                    <div className="p-6 mt-12 bg-[#10071f] z-50" style={{ borderBottomLeftRadius: "50%" }}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu">
                             <line x1="4" x2="20" y1="12" y2="12" />
                             <line x1="4" x2="20" y1="6" y2="6" />
@@ -107,11 +83,11 @@ export const NavigationMenu = () => {
                 <DrawerContent
                     aria-describedby={undefined}
                     onInteractOutside={() => setOpen(false)}
-                    className={cn(isFixed && "fixed top-0", "bg-[#10071f]")}
+                    className="fixed top-0 bg-[#10071f]"
                 >
-                    <div className="my-6 overflow-y-auto">
+                    <div className="overflow-y-auto">
                         <DrawerTitle className='hidden' />
-                        <Navigation onClick={() => setOpen(false)} open={open} isFixed={isFixed} />
+                        <Navigation onClick={() => setOpen(false)} />
                     </div>
                     <StarsBackground className="-z-10" />
                     <ShootingStars className="-z-10" />
